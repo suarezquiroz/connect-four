9d50bf489787080dc65c1e2527e20230e518002b
Santiago Suarez <suarezquiroz@gmail.com>
2019-07-31 17:34:11 -0500

    win condition function

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -9,6 +9,8 @@ export class GameBoardComponent implements OnInit {
   private n = 4;
   columns = 7;
   rows = 6;
+  currentColor = 'red';
+  winner = '';
 
   @Input()
   get size(): number {
@@ -29,18 +31,81 @@ export class GameBoardComponent implements OnInit {
     // generate matrix
     // 2N-1 x N+2 board, 5N+1 piece, 4 <= N <= 10
     this.board = [
-      ['none', 'none', 'none', 'none', 'none', 'red', 'red'],
-      ['none', 'none', 'none', 'none', 'none', 'red', 'yellow'],
-      ['none', 'none', 'none', 'none', 'none', 'red', 'yellow'],
-      ['none', 'none', 'none', 'none', 'none', 'yellow', 'red'],
-      ['none', 'none', 'none', 'none', 'none', 'yellow', 'red'],
-      ['none', 'none', 'none', 'none', 'none', 'red', 'yellow']
+      ['none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none']
     ];
-    console.log('this.board', this.board);
 
   }
 
   addDisc(column: number, color: string) {
+    const emptySlot = this.board[column].lastIndexOf('none');
+    if (emptySlot !== -1) {
+      this.board[column][emptySlot] = this.currentColor;
+      this.checkConnect4();
+      this.switchColor();
+    }
+  }
+
+  switchColor() {
+    if (this.currentColor === 'red') {
+      this.currentColor = 'yellow';
+    } else {
+      this.currentColor = 'red';
+    }
+  }
+
+  checkConnect4() {
+    let matches = 0;
+    const colorLayer = this.board.map(column => column.map(slot => slot === this.currentColor).reverse());
+
+    const checkDirection = (posX: number, posY: number, dirX: number, dirY: number) => {
+      if (colorLayer[posX + dirX] && colorLayer[posX + dirX][posY + dirY] === true) {
+        matches++;
+        if (matches >= this.n) {
+          return true;
+        } else {
+          return checkDirection(posX + dirX, posY + dirY, dirX, dirY);
+        }
+      } else {
+        matches = 1;
+        return false;
+      }
+    };
+
+    for (let col = 0; col < colorLayer.length; col++) {
+      const column = colorLayer[col];
+      for (let row = 0; row < column.length; row++) {
+        const slot = column[row];
+        if (slot) {
+          matches = 1;
+
+          if (
+            // check left
+            checkDirection(col, row, -1, 0) ||
+            // check down left
+            checkDirection(col, row, -1, 1) ||
+            // check down
+            checkDirection(col, row, 0, 1) ||
+            // check down right
+            checkDirection(col, row, 1, 1) ||
+            // check right
+            checkDirection(col, row, 1, 0)
+          ) {
+            console.log('colorLayer', colorLayer);
+            this.winner = this.currentColor;
+            break;
+          }
+        }
+      }
+      if (this.winner) {
+        break;
+      }
+    }
 
   }
 
